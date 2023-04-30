c22838cbd75dba39b236a6c04d64334e2f71d08e
vazeraja <vazeraja@gmail.com>
2023-04-30 11:42:21 -0400

    menu change

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,19 +4,23 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField] private InputActionAsset inputActionAsset;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject rulesMenu;
     
     [SerializeField] private Button startButton;
     [SerializeField] private Button rulesButton;
     [SerializeField] private Button exitButton;
-    
+
     private void OnEnable()
     {
+        inputActionAsset.Disable();
+        
         startButton.onClick.AddListener(OnStart);
         rulesButton.onClick.AddListener(OnRules);
         exitButton.onClick.AddListener(OnExit);
@@ -27,12 +31,12 @@ public class UIController : MonoBehaviour
         startButton.onClick.RemoveListener(OnStart);
         rulesButton.onClick.RemoveListener(OnStart);
         exitButton.onClick.RemoveListener(OnExit);
-
     }
 
     private void OnStart()
     {
         gameObject.SetActive(false);
+        inputActionAsset.Enable();
     }
     private void OnRules()
     {
